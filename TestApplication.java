59414cb8a39674d5a9b68888280e93bdf8b8d65d
Henry409 <73369619+Henry409@users.noreply.github.com>
2022-02-01 12:46:27 -0600

    Add files via upload

---
+++ TestApplication.java
@@ -0,0 +1,8 @@
+
+public class TestApplication {
+	public static void main(String[] args) {
+		System.out.println("Just a basic program to see if I can upload to git");
+		System.out.println("Its asking for a change so here it is");
+			
+	}
+}
